698639c5cd2a1a03acd81ba393e63de9021c25bc
Rebecca Nhan <rebecca_nhan@rogers.com>
2022-11-05 15:49:07 -0400

    Added filtering books by alphabetical/chronological

--- backend/controllers/bookController.js
+++ backend/controllers/bookController.js
@@ -1,21 +1,19 @@
 const asyncHandler = require('express-async-handler')
 const Book = require('../models/bookModel')
 
-const DESC = 'desc' // descending order
-const ASC = 'asc' // ascending order
-const ALPHA = 'alpha' // alphabetical order
-const CHRON = 'chron' // chronological order (by year)
+const DESC = 'desc'
+const ASC = 'asc'
+const YEAR = 'year' // alphabetical order
+const TITLE = 'uk_title' // chronological order (by year)
 
 // @desc Get all books in the database
 // @route GET /api/books
 // @access Public
 const getBooks = asyncHandler(async (req, res) => {
-    // console.log(req.query)
-    // order = req.query.order
-    if (order) {
-        console.log(order)
-    }
-    const books = await Book.find()
+    orderby = req.query.orderby || ASC
+    ordertype = req.query.ordertype || YEAR
+
+    const books = await Book.find().sort({ [ordertype] : orderby })
     res.status(200).json(books)
 })
 
