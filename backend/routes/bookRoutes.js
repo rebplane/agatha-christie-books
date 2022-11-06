f18d8a24e3417cf88d0a546ce64765fdf6e74332
Rebecca Nhan <rebecca_nhan@rogers.com>
2022-11-05 20:54:16 -0400

    Removed unnecessary import

--- backend/routes/bookRoutes.js
+++ backend/routes/bookRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const { getBooks, getBookById, getBooksByCharacter } = require('../controllers/bookController')
+const { getBooks, getBookById } = require('../controllers/bookController')
 
 router.get('/', getBooks)
 
