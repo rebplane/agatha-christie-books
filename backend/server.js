de00547dd863455bae7fcfb21e7bdaef68d457f8
Rebecca Nhan <rebecca_nhan@rogers.com>
2022-11-09 23:01:33 -0500

    Preparing for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 const dotenv = require('dotenv').config()
 const mongoose = require('mongoose')
 const port = process.env.PORT  || 5000
@@ -31,5 +32,11 @@ passport.deserializeUser(User.deserializeUser());
 
 app.use('/api/accounts', jsonParser, require('./routes/accountRoutes'))
 app.use('/api/books', jsonParser, require('./routes/bookRoutes'))
+
+// Serve frontend
+if (process.env.NODE_ENV == 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')))
+}
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
